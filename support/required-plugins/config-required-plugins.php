9d89eb30c44900df7fcd29169cf1da59d3db7fc2
corsacca <corsacca@gmail.com>
2021-10-11 10:12:09 +0100

    Remove duplicate tgmpa class declaration

--- support/required-plugins/config-required-plugins.php
+++ support/required-plugins/config-required-plugins.php
@@ -37,30 +37,32 @@ add_action( 'tgmpa_register', function () {
      * If the source is NOT from the .org repo, then source is also required.
      */
     $plugins = array(
-//        array(
-//            'name'                  => 'Disciple.Tools - Custom Login',
-//            'slug'                  => 'disciple-tools-custom-login',
-//            'required'              => false,
-//            'version'               => '0.1',
-//        ),
+        //        array(
+        //            'name'                  => 'Disciple.Tools - Custom Login',
+        //            'slug'                  => 'disciple-tools-custom-login',
+        //            'required'              => false,
+        //            'version'               => '0.1',
+        //        ),
         array(
             'name'                  => 'Disciple.Tools - Webform',
             'slug'                  => 'disciple-tools-webform',
+            'source'                => 'https://github.com/DiscipleTools/disciple-tools-webform/releases/latest/download/disciple-tools-webform.zip',
             'required'              => false,
             'version'               => '4.4',
         ),
         array(
             'name'                  => 'Disciple.Tools - Prayer Campaigns',
             'slug'                  => 'disciple-tools-prayer-campaigns',
+            'source'                => 'https://github.com/DiscipleTools/disciple-tools-prayer-campaigns/releases/latest/download/disciple-tools-prayer-campaigns.zip',
             'required'              => true,
             'version'               => '0.1',
         ),
-//        array(
-//            'name'                  => 'Genesis Blocks',
-//            'slug'                  => 'genesis-blocks',
-//            'required'              => false,
-//            'version'               => '1.2.5',
-//        ),
+        //        array(
+        //            'name'                  => 'Genesis Blocks',
+        //            'slug'                  => 'genesis-blocks',
+        //            'required'              => false,
+        //            'version'               => '1.2.5',
+        //        ),
     );
 
     /*
@@ -70,94 +72,7 @@ add_action( 'tgmpa_register', function () {
      */
     $config = array(
         'id'           => 'p4r_porch',                 // Unique ID for hashing notices for multiple instances of TGMPA.
-        'default_path' => '/includes/plugins/',     // Default absolute path to bundled plugins.
-        'menu'         => 'tgmpa-install-plugins', // Menu slug.
-        'parent_slug'  => 'plugins.php',            // Parent menu slug.
-        'capability'   => 'manage_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
-        'has_notices'  => true,                    // Show admin notices or not.
-        'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
-        'dismiss_msg'  => 'These are recommended plugins to complement your Disciple.Tools system.',                      // If 'dismissable' is false, this message will be output at top of nag.
-        'is_automatic' => true,                   // Automatically activate plugins after installation or not.
-        'message'      => '',                      // Message to output right before the plugins table.
-
-        /*
-        'strings'      => array(
-            'page_title'                      => __( 'Install Required Plugins', 'pray4ramadan-porch' ),
-            'menu_title'                      => __( 'Install Plugins', 'pray4ramadan-porch' ),
-            /* translators: %s: plugin name. * /
-            'installing'                      => __( 'Installing Plugin: %s', 'pray4ramadan-porch' ),
-            /* translators: %s: plugin name. * /
-            'updating'                        => __( 'Updating Plugin: %s', 'pray4ramadan-porch' ),
-            'oops'                            => __( 'Something went wrong with the plugin API.', 'pray4ramadan-porch' ),
-            'notice_can_install_required'     => _n_noop(
-                /* translators: 1: plugin name(s). * /
-                'This theme requires the following plugin: %1$s.',
-                'This theme requires the following plugins: %1$s.',
-                'pray4ramadan-porch'
-            ),
-            'notice_can_install_recommended'  => _n_noop(
-                /* translators: 1: plugin name(s). * /
-                'This theme recommends the following plugin: %1$s.',
-                'This theme recommends the following plugins: %1$s.',
-                'pray4ramadan-porch'
-            ),
-            'notice_ask_to_update'            => _n_noop(
-                /* translators: 1: plugin name(s). * /
-                'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
-                'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
-                'pray4ramadan-porch'
-            ),
-            'notice_ask_to_update_maybe'      => _n_noop(
-                /* translators: 1: plugin name(s). * /
-                'There is an update available for: %1$s.',
-                'There are updates available for the following plugins: %1$s.',
-                'pray4ramadan-porch'
-            ),
-            'notice_can_activate_required'    => _n_noop(
-                /* translators: 1: plugin name(s). * /
-                'The following required plugin is currently inactive: %1$s.',
-                'The following required plugins are currently inactive: %1$s.',
-                'pray4ramadan-porch'
-            ),
-            'notice_can_activate_recommended' => _n_noop(
-                /* translators: 1: plugin name(s). * /
-                'The following recommended plugin is currently inactive: %1$s.',
-                'The following recommended plugins are currently inactive: %1$s.',
-                'pray4ramadan-porch'
-            ),
-            'install_link'                    => _n_noop(
-                'Begin installing plugin',
-                'Begin installing plugins',
-                'pray4ramadan-porch'
-            ),
-            'update_link'                     => _n_noop(
-                'Begin updating plugin',
-                'Begin updating plugins',
-                'pray4ramadan-porch'
-            ),
-            'activate_link'                   => _n_noop(
-                'Begin activating plugin',
-                'Begin activating plugins',
-                'pray4ramadan-porch'
-            ),
-            'return'                          => __( 'Return to Required Plugins Installer', 'pray4ramadan-porch' ),
-            'plugin_activated'                => __( 'Plugin activated successfully.', 'pray4ramadan-porch' ),
-            'activated_successfully'          => __( 'The following plugin was activated successfully:', 'pray4ramadan-porch' ),
-            /* translators: 1: plugin name. * /
-            'plugin_already_active'           => __( 'No action taken. Plugin %1$s was already active.', 'pray4ramadan-porch' ),
-            /* translators: 1: plugin name. * /
-            'plugin_needs_higher_version'     => __( 'Plugin not activated. A higher version of %s is needed for this theme. Please update the plugin.', 'pray4ramadan-porch' ),
-            /* translators: 1: dashboard link. * /
-            'complete'                        => __( 'All plugins installed and activated successfully. %1$s', 'pray4ramadan-porch' ),
-            'dismiss'                         => __( 'Dismiss this notice', 'pray4ramadan-porch' ),
-            'notice_cannot_install_activate'  => __( 'There are one or more required or recommended plugins to install, update or activate.', 'pray4ramadan-porch' ),
-            'contact_admin'                   => __( 'Please contact the administrator of this site for help.', 'pray4ramadan-porch' ),
-
-            'nag_type'                        => '', // Determines admin notice type - can only be one of the typical WP notice classes, such as 'updated', 'update-nag', 'notice-warning', 'notice-info' or 'error'. Some of which may not work as expected in older WP versions.
-        ),
-        */
     );
 
     tgmpa( $plugins, $config );
-    }
-);
+});
